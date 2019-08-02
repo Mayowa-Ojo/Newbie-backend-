7a206f9f3b552e854093ca7a5cfdb46a98480789
Mayowa Ojo <mayowaojo.e@gmail.com>
2019-08-02 22:35:14 +0100

    connect server to database

---
+++ src/public/assets/seedData.js
@@ -0,0 +1,18 @@
+const posts = [
+  {
+    title: 'Challenges I faced as a toddler in the tech world',
+    body: `Lyft tempor tilde chartreuse quinoa. Waistcoat 8-bit messenger bag, prism commodo blue bottle mlkshk chicharrones quis pug gluten-free retro pariatur affogato. Subway tile plaid taiyaki PBR&B. Labore chambray edison bulb pug hoodie sint laboris readymade deep v kombucha you probably haven't heard of them. Butcher hashtag tote bag man braid, squid meggings fingerstache. Mlkshk tacos intelligentsia, put a bird on it dolor squid helvetica single-origin coffee.
+
+    Celiac id PBR&B poke ramps tumeric yuccie art party tilde beard. Next level poutine unicorn fam occaecat. Organic adipisicing in twee vape pop-up gastropub sartorial selfies readymade helvetica narwhal enim williamsburg man braid. Chia occupy in neutra shabby chic, in la croix bespoke 8-bit anim dreamcatcher. Venmo occupy viral pitchfork, chicharrones VHS selvage migas hammock tousled sriracha bushwick franzen in lomo. Roof party listicle aesthetic elit, vinyl letterpress celiac brooklyn butcher mixtape XOXO everyday carry organic echo park cloud bread. Nulla normcore +1 voluptate drinking vinegar swag eiusmod in polaroid lo-fi.
+    
+    Copper mug ex 90's cillum ullamco id qui man bun pickled beard. Vape qui edison bulb nulla locavore vexillologist unicorn seitan prism polaroid kickstarter forage thundercats aliquip cardigan. Nisi photo booth selvage four loko duis laboris swag, nulla irony typewriter irure pinterest. Biodiesel laborum drinking vinegar meditation artisan sed +1 cloud bread dolor anim. Tbh magna fashion axe artisan. Elit ad semiotics cloud bread iceland hammock. In chartreuse jianbing, letterpress irony commodo intelligentsia dreamcatcher excepteur venmo fingerstache velit.
+    
+    Glossier drinking vinegar DIY, deserunt marfa quinoa tote bag before they sold out man braid direct trade. Tempor kogi enim tousled ut tilde, crucifix do literally cardigan ex fixie tacos drinking vinegar. Crucifix DIY fixie quinoa next level non pop-up authentic officia church-key gastropub put a bird on it plaid before they sold out seitan. Venmo man braid fanny pack officia jianbing laboris freegan deserunt laborum pour-over, keytar ugh. Tempor cronut paleo, master cleanse af shoreditch godard ennui. Keffiyeh unicorn succulents, anim bespoke austin vinyl cliche. Austin cardigan ad fixie tofu.
+    
+    YOLO ut distillery nisi sed, bushwick readymade celiac mustache try-hard microdosing forage. Meh exercitation vinyl, nulla leggings aliqua vexillologist consequat ramps. Fashion axe fanny pack adipisicing ethical pinterest af shabby chic iceland pok pok. Ipsum normcore heirloom prism, taxidermy brunch single-origin coffee edison bulb pitchfork godard.`,
+    author: 'Brad Travolta',
+    likes: 0
+  }
+]
+
+module.exports = posts;
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,22 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/post');
-const { getSinglePost } = require('../middlewares/posts')
+const { getSinglePost } = require('../middlewares/posts');
+const seedData = require('../public/assets/seedData');
 
 /* setup posts routes */
 /* get all posts */
-router.get('/', async (req, res) => {
-  try {
-    const posts = await Post.find()
-    res.json(posts);
-  } catch(err) {
-    res.status(500).json({message: err.message});
-  }
-})
+// router.get('/', async (req, res) => {
+//   try {
+//     const posts = await Post.find()
+//     res.json(posts);
+//   } catch(err) {
+//     res.status(500).json({message: err.message});
+//   }
+// })
 
 /* get one post */
 router.get('/:id', getSinglePost, (req, res) => {
   res.json(res.post);
+})
+
+/* create a post */
+router.get('/', async (req, res) => {
+  const post = new Post(seedData[0]);
+  try {
+    const newPost = await post.save();
+    res.status(201).json(newPost);
+  } catch(err) {
+    res.status(400).json({message: err.message});
+  }
 })
 
 module.exports = router;
