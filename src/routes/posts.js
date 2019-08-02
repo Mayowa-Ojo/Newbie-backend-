dcb658423c6dfb3a106610867d13acf0b6066009
Mayowa Ojo <mayowaojo.e@gmail.com>
2019-08-02 22:59:11 +0100

    update

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -6,29 +6,29 @@ const seedData = require('../public/assets/seedData');
 
 /* setup posts routes */
 /* get all posts */
-// router.get('/', async (req, res) => {
-//   try {
-//     const posts = await Post.find()
-//     res.json(posts);
-//   } catch(err) {
-//     res.status(500).json({message: err.message});
-//   }
-// })
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find()
+    res.json(posts);
+  } catch(err) {
+    res.status(500).json({message: err.message});
+  }
+})
 
 /* get one post */
 router.get('/:id', getSinglePost, (req, res) => {
   res.json(res.post);
 })
 
-/* create a post */
-router.get('/', async (req, res) => {
-  const post = new Post(seedData[0]);
-  try {
-    const newPost = await post.save();
-    res.status(201).json(newPost);
-  } catch(err) {
-    res.status(400).json({message: err.message});
-  }
-})
+// /* create a post */
+// router.get('/', async (req, res) => {
+//   const post = new Post(seedData[0]);
+//   try {
+//     const newPost = await post.save();
+//     res.status(201).json(newPost);
+//   } catch(err) {
+//     res.status(400).json({message: err.message});
+//   }
+// })
 
 module.exports = router;
