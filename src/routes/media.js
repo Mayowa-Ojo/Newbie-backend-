0cec0175920806fbe44188ef4eabcb453edde6d1
Mayowa Ojo <mayowaojo.e@gmail.com>
2019-10-04 07:49:45 +0100

    authorize image routes

--- src/routes/media.js
+++ src/routes/media.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const passport  = require('passport');
 /** Relative imports */
 const { parser } = require('../config/media-upload');
 const { 
@@ -9,10 +9,13 @@ const {
   deleteMedia 
 } = require('../controllers/media-controller');
 
+/** global variables */
+const authorizeRoute = passport.authenticate('jwt', { session: false });
+
 router.get('/:id', getMedia);
 
-router.post('/', parser.single('image'), createMedia);
+router.post('/', authorizeRoute, parser.single('image'), createMedia);
 
-router.delete('/:id', deleteMedia);
+router.delete('/:id', authorizeRoute, deleteMedia);
 
 module.exports = router;
